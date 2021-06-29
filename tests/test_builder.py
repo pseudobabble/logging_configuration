9b0272d88344f89a0208665e4e374be0fdd3f71d
Harry Johnson <harry.johnson@eigentech.com>
2021-06-29 18:16:15 +0100

    Basic LoggingConfigurationBuilder

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+import pytest
+
+from logging_configuration.builder import LoggingConfigurationBuilder
+
+def test__build_formatters_builds_correctly():
+    builder = LoggingConfigurationBuilder()
